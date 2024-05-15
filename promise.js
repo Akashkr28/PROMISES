ead8e18880aaa3fd9754cd58878ac16546eebd86
Akashkr28 <email.akash2010@gmail.com>
2024-05-15 22:26:39 +0530

    promise

--- promise.js
+++ promise.js
@@ -2,10 +2,23 @@ const promiseOne = new Promise(function(resolve, reject){
     // Do an Async task
     // DB calls, cryptography, networkcalls
     setTimeout(function(){
-        console.log('Async task is completed')
+        console.log('Async task is completed');
+        resolve()
     }, 1000)
 })
 
 promiseOne.then(function(){
     console.log("Promise Consumed")
+})
+
+
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async Task Two");
+        resolve()
+    }, 1000)
+
+}).then(function(){
+    console.log("Async Two resolved")
 })
